86a3de3ff9fd417e330db2bde58754100363c18b
Ali Jawaid <iamalimemon11@gmail.com>
2024-12-07 20:58:56 +0500

    second

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,5 @@
-<<<<<<< HEAD
-import Image from "next/image";
-=======
+
 import Navbar from "./components/Navbar";
->>>>>>> 7917ca908ce95922858242bbc97b83a8c56d2545
 
 export default function Home() {
   return (
